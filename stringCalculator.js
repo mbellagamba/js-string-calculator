2cd2d31df3240e58c948a0177ca151995d446461
Lorenzo Carducci <lorenzo.carducci@nautes.com>
2019-04-24 16:07:09 +0200

    Add negative number exception test case

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,16 +1,33 @@
+function NegativeNumberException(values) {
+    this.message = 'You cannot insert negative numbers';
+    this.values = values;
+}
+
+NegativeNumberException.prototype = Object.create(Error.prototype);
+
 function add(numbers) {
   if (!numbers) return 0;
 
   if (numbers.startsWith('//')) {
-    customDelimiter = numbers.substring(2, 3);
+    customDelimiter = numbers[2];
     numbers = numbers.substring(4).split(customDelimiter).join(',');
   }
 
-  return numbers
+  const negativeNumbers = [];
+
+  const sum = numbers
     .replace(/\n|\r\n/g, ',')
     .split(',')
-    .map(s => parseInt(s))
+    .map(s => {
+      const number = parseInt(s);
+      if (number < 0) negativeNumbers.push(number);
+      return number;
+    })
     .reduce((acc, n) => acc + n);
+
+  if (negativeNumbers.length > 0) throw new NegativeNumberException(negativeNumbers);
+
+  return sum;
 }
 
 module.exports = {
